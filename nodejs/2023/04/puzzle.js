fbe5d31a2ffea739e17f4b8008bad691863558fb
Mark <mark@brightenfield.com>
2023-12-04 03:48:19 -0500

    Adding Puzzle 4

--- nodejs/2023/04/puzzle.js
+++ nodejs/2023/04/puzzle.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
-const util = require("util");
 
 let filename = process.argv[2];
 let rows = fs.readFileSync(filename, "utf-8").split("\r\n");
@@ -8,8 +7,8 @@ let part2_sum = 0;
 
 let cards = rows.map(row => {
     let {id, winning_nums, your_nums} = [...row.matchAll(/Card\s+(?<id>\d+):\s+(?<winning_nums>(\d+\s+)+)\|\s+(?<your_nums>(\d+\s*)+)/ig)][0].groups;
-    winning_nums = winning_nums.trim().split(/\s+/ig);
-    your_nums = your_nums.trim().split(/\s+/ig);
+    winning_nums = winning_nums.trim().split(/\s+/);
+    your_nums = your_nums.trim().split(/\s+/);
 
     let matches = winning_nums.filter(x => your_nums.includes(x));
     
@@ -29,9 +28,6 @@ recursiveWin = ({id, matches}) => {
 
 cards.map(x => recursiveWin(x));
 part2_sum = cards.map(x => x.copies).reduce((a,b) => a+b, 0);
-
-//console.log(util.inspect(cards));
-//console.log(cards.map(({id, copies}) => copies));
 
 console.log(`Part 1 Sum: ${part1_sum}`);
 console.log(`Part 2 Sum: ${part2_sum}`);
